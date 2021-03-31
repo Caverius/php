3aaea3afe8a8551e638cd22bae291a3e7fcbbe04
Caverius <j.s.ramos1994@gmail.com>
2021-03-30 21:03:54 -0300

    cambios

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+$miArray = array();
+$miArray[0]= "jj";
+$miArray[1]= "ss";
+$miArray[2]= "jsr";
+print_r($miArray)
+
+
+
+?>
+<?php
+$Nombre= "Juan";
+$Apellido= "Ramos";
+
+echo "Me llamo $Nombre $Apellido";
+?>
---
+++ stock.php
@@ -0,0 +1,11 @@
+<?php
+$stock= 800;
+
+
+if ($stock>0){
+echo "Hay STOCK";
+}
+else {
+echo "No hay Stock";
+}
+ ?>
